c5795cba23e137dbcac8138c7f61a941b0060071
Chanel Donahoe <cdonahoe05@gmail.com>
2021-07-14 23:09:34 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+   return customerName=customerName.toUpperCase();
+}
+
+// var bestCustomer = 'not bob'
+
+function setBestCustomer() {
+   return  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'chanel'
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'bob';
+}
